af7a071098332f67839a98517f0a9faf205b2242
wzxlll33 <wzx20041108@gmail.com>
2026-05-10 08:11:02 +0800

    Initial project submission

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+// 加这三行调试
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
 define('ROOT_PATH', dirname(__DIR__));
 
 spl_autoload_register(function (string $class): void {
